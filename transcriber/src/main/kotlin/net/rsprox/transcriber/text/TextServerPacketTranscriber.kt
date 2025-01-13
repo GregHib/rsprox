2f8d7f339715f444f573b2c592aae7945d3947ae
GregHib <greghib@users.noreply.github.com>
2025-01-13 01:46:22 +0000

    Add code printing command

--- transcriber/src/main/kotlin/net/rsprox/transcriber/text/TextServerPacketTranscriber.kt
+++ transcriber/src/main/kotlin/net/rsprox/transcriber/text/TextServerPacketTranscriber.kt
@@ -2675,8 +2675,8 @@ public class TextServerPacketTranscriber(
         root.formattedInt("weight", message.runweight, KG_NUMBER_FORMAT)
     }
 
-    private enum class Stat(
-        val id: Int,
+    public enum class Stat(
+        public val id: Int,
     ) : NamedEnum {
         ATTACK(0),
         DEFENCE(1),
